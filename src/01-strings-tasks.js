f7e275460002bae13aa07c7cd658a524f3a458ae
Maxim <104011221+maximozaitsev@users.noreply.github.com>
2023-08-30 16:12:09 +0300

    11 strings tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -168,8 +168,8 @@ function unbracketTag(str) {
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function convertToUpperCase(/* str */) {
-  throw new Error('Not implemented');
+function convertToUpperCase(str) {
+  return str.toUpperCase();
 }
 
 /**
